c4ccf428c23578670cf3ed4e970be25a1f797101
maharshi66 <40323681+maharshi66@users.noreply.github.com>
2024-12-06 13:30:54 -0800

    Fixed missing ValidationPipe in api-gateway bootstrap function

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
@@ -9,6 +10,13 @@ async function bootstrap() {
   app.use(helmet());
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }),
+  );
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
 }
